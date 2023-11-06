52663b87ec9ee9761f424e6c4e1bec88481e86d3
guido1992 <rathkealex@gmail.com>
2023-11-06 22:13:58 +0000

    Add files via upload

--- Skills.py
+++ Skills.py
@@ -5,27 +5,80 @@ Created on Thu Nov  2 11:43:02 2023
 @author: rathk
 """
 
+### ----- SKILL SELECTION APPLICATION -----
+
+# Import Libraries
 import streamlit as st
 import pandas as pd
 
+# Read data file
 data = pd.read_csv(r'C:/Users/rathk/OneDrive/Desktop/test_sample/skills.csv')
 
-# Set the title and a brief description for your app
+### ----- SET TITLE & DESCRIPTION FOR APPLICATION -----
 st.title("Skill Set Finder")
 
+# Add an image to the top left of the application
+st.write("""
+         ***Add image to app***
+         """)
+#st.image("C:/Users/rathk/OneDrive/Desktop/test_sample/CSLogo.jpeg", use_column_width=False)
+
+st.write("""
+         We have a new client and want to find resources in our employee pool for this new project. Below is a sample dataset to
+         illustrate the workings of this application.
+         """)
+
+st.write("""
+         Using this dataset we want to find employees with specific skills and levels of expertise that are available to work on this
+         project.
+         """)
+
+# Display dataframe
 st.write(data)
 
-st.write("Select a skill and expertise level to filter individuals:")
+#### ----- FORMAT APPLICATION -----
 
-# Create a multi-select widget for skill selection
-selected_skills = st.multiselect("Select Skills", data['Skill'].unique())
+# Set Filter
+st.subheader("***Filter selection***")
 
-# Create a selection widget for expertise level
-selected_level = st.selectbox("Select Expertise Level", data['Expertise'].unique())
+# Description
+st.write("Select a skill and expertise level to find individuals for specific projects.")
+
+# Create three columns for Skill 1, Skill 2 and Skill 3 selections
+col1, col2, col3 = st.columns(3)
+
+# Within the first column, create multi-select widgets for Skill 1
+with col1:
+    selected_skill_1 = st.multiselect("**Select Skill 1**", data['Skill'].unique())
+    selected_level_1 = st.multiselect("**Select Expertise Level for Skill 1**", data['Expertise'].unique())
+
+# Within the second column, create multi-select widgets for Skill 2
+with col2:
+    selected_skill_2 = st.multiselect("**Select Skill 2**", data['Skill'].unique())
+    selected_level_2 = st.multiselect("**Select Expertise Level for Skill 2**", data['Expertise'].unique())
+
+# Within the third column, create multi-select widgets for Skill 3
+with col3:
+    selected_skill_3 = st.multiselect("**Select Skill 3**", data['Skill'].unique())
+    selected_level_3 = st.multiselect("**Select Expertise Level for Skill 3**", data['Expertise'].unique())
 
 # Filter the DataFrame based on user selections
-filtered_data = data[(data['Skill'].isin(selected_skills)) & (data['Expertise'] == selected_level)]
+filtered_data = data[
+    (data['Skill'].isin(selected_skill_1) & data['Expertise'].isin(selected_level_1)) |
+    (data['Skill'].isin(selected_skill_2) & data['Expertise'].isin(selected_level_2)) |
+    (data['Skill'].isin(selected_skill_3) & data['Expertise'].isin(selected_level_3))
+]
 
-# Display the filtered DataFrame
-st.write("Filtered Data:")
-st.write(filtered_data)
+### ----- DISPLAY THE INDIVIDUALS WHO FIT THE SKILL SELECTION -----
+
+# Outcome
+st.subheader("***Outcome view***")
+
+# Add a statement about the selected individuals for the project
+if not filtered_data.empty:
+    st.write("You have selected the following individuals for your project:")
+    
+    for index, row in filtered_data.iterrows():
+        st.write(f"- **{row['Person']}** has expertise in {row['Skill']} at the {row['Expertise']} level.")
+else:
+    st.write("No individuals found with the selected skillsets for your project.")
